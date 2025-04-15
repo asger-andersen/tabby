3eb2e3516a4a418c6523705416c9ba937c442828
asger-andersen <the.cave.of.affiliates@gmail.com>
2025-04-15 23:24:18 +0200

    Fixed cors policy

--- backend/api/app.js
+++ backend/api/app.js
@@ -9,9 +9,15 @@ const PORT = 8000;
 const app = express();
 app.use(express.json());
 
-const allowedOrigins = ['https://tabbyapp.vercel.app/'];
+const allowedOrigins = ['https://tabbyapp.vercel.app'];
 
 app.use(cors({
+  origin: allowedOrigins,
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+}));
+
+app.options('*', cors({
   origin: allowedOrigins,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
